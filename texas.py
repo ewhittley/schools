db859f7dfb34c9742ddb1bcf6c829007912d1b98
ewhittley <eric.whittley@gmail.com>
2017-04-30 08:33:46 -0500

    Add texas python to call teadownloader

--- texas.py
+++ texas.py
@@ -1,19 +1,14 @@
 import os
 import convert_to_json
+import teadownloader
 
 
 def download_state_data():
-	print 'unable to download anything'
-
-def get_state_data(state):
-	current_directory = os.path.abspath(os.path.dirname( __file__ ))
-	state_data_directory = os.path.join(os.path.join(current_directory, 'resources')
-
-	print state_data_directory
+	teadownloader.get_multiple_years('district', '1999-2001', 'data')
 
 def convert_data(state, file_name):
 	file_name = ''
 
 if __name__ == '__main__':
 	# convert_to_json.csv_to_json('district.dat', 'y')
-	get_state_data('texas')
+	download_state_data()
